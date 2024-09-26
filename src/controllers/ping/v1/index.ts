451ee7003af9424ef44b2ed30e68ce65cf9fc26a
Sarthak-code360 <sarhakmishra3032002@gmail.com>
2024-09-26 14:35:28 +0530

    testing server setup

--- src/controllers/ping/v1/index.ts
+++ src/controllers/ping/v1/index.ts
@@ -5,7 +5,7 @@ import { PingRequest, PingResponse } from '../../../grpc/ping/v1/ping_pb.js';
 
 const ping: MethodImpl<typeof PingService.methods.ping> = (req: PingRequest, context: HandlerContext): PingResponse => {
   return new PingResponse({
-    message: `Pong ${req.message}`,
+    message: `Pong ${req.message}, ${req.name}`,
   });
 };
 
