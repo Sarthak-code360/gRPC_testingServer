4be5df08f4802b60c98acbb8f22f35d44e74ff31
Sarthak-code360 <sarhakmishra3032002@gmail.com>
2024-09-30 15:11:40 +0530

    Added new variable in request

--- src/controllers/ping/v1/index.ts
+++ src/controllers/ping/v1/index.ts
@@ -5,7 +5,7 @@ import { PingRequest, PingResponse } from '../../../grpc/ping/v1/ping_pb.js';
 
 const ping: MethodImpl<typeof PingService.methods.ping> = (req: PingRequest, context: HandlerContext): PingResponse => {
   return new PingResponse({
-    message: `Pong ${req.message}, ${req.name}`,
+    message: `Pong ${req.message}, ${req.name}, ${req.device_name}, ${req.curr}`,
   });
 };
 
